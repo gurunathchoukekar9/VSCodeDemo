97505265eb98b2be5a5aec7180b7616242513461
Gurunath Choukekar <gurunath.choukekar@hpe.com>
2021-07-23 09:28:33 -0600

    welcome message modified

---
+++ 1-Git Explorer/git-example.py
@@ -0,0 +1,8 @@
+
+#git add --> Stage 
+#git commit --> commit  (All in one command if you don't like Git a lot)
+#git push
+#All other menu tabs --> git pull,git push,git branch,git tag
+
+
+print("Welcome to VSCode DEMO..")
